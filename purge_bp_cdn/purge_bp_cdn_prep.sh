644683748ed4b38d9ef0c872537f1e50d944d878
CalvinLeung21 <courier_ciders_9u@icloud.com>
2025-06-04 17:41:25 +0800

    Update purge_bp_cdn_prep.sh

--- purge_bp_cdn/purge_bp_cdn_prep.sh
+++ purge_bp_cdn/purge_bp_cdn_prep.sh
@@ -1,4 +1,4 @@
-if ! python3 -c "import byteplus_sdk" &>/dev/null; then
+if ! python -c "import byteplus_sdk" &>/dev/null; then
   [[ $(id -u) -eq 0 ]] && sudo_opts= || sudo_opts="--user"
-  python3 -m pip install $sudo_opts --upgrade wheel byteplus-sdk
+  python -m pip install $sudo_opts --upgrade wheel byteplus-sdk
 fi
