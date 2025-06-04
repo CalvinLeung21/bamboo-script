869b33a6ff9a1bfd79e3a6e29f510bbd84fe131d
CalvinLeung21 <courier_ciders_9u@icloud.com>
2025-06-04 17:50:28 +0800

    Update purge_bp_cdn_prep.sh

--- purge_bp_cdn/purge_bp_cdn_prep.sh
+++ purge_bp_cdn/purge_bp_cdn_prep.sh
@@ -1,4 +1,13 @@
-if ! python -c "import byteplus_sdk" &>/dev/null; then
-  [[ $(id -u) -eq 0 ]] && sudo_opts= || sudo_opts="--user"
-  python -m pip install $sudo_opts --upgrade wheel byteplus-sdk
+# Ensure pip is installed
+if ! python3 -m pip -V &>/dev/null; then
+  python3 -m ensurepip --upgrade || {
+    echo "pip is not available and cannot be bootstrapped. Please install it manually."
+    exit 1
+  }
+fi
+
+# Install byteplus_sdk if not available
+if ! python3 -c "import byteplus_sdk" &>/dev/null; then
+  opts=$( [ "$(id -u)" -eq 0 ] && echo "" || echo "--user" )
+  python3 -m pip install $opts --upgrade wheel byteplus-sdk
 fi
