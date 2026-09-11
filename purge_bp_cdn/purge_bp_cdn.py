7c9526e798edefcd10617de93527d8d8cae52bc6
CalvinLeung21 <courier_ciders_9u@icloud.com>
2026-09-11 21:36:14 +0800

    Modify purge script for multiple URL support
    Updated script to accept multiple URLs and improved usage message.

--- purge_bp_cdn/purge_bp_cdn.py
+++ purge_bp_cdn/purge_bp_cdn.py
@@ -1,20 +1,27 @@
-#  -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../")
-# from byteplus_sdk.example.cdn import ak, sk
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../../")
+
 from byteplus_sdk.cdn.service import CDNService
 
-# print (sys.arg)
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print("Usage: python script.py <ak> <sk> <url>")
+        print("Usage: python purge_bp_cdn.py <ak> <sk> <url1> [url2] [url3] [url4]")
         sys.exit(1)
 
     ak = sys.argv[1]
     sk = sys.argv[2]
-    url = sys.argv[3]
+    urls = sys.argv[3:]
+
+    if len(urls) > 4:
+        print("Error: Maximum 4 URLs can be submitted at once.")
+        print("Received:", len(urls))
+        sys.exit(1)
+
+    urls_string = "\n".join(urls)
 
     svc = CDNService()
     svc.set_ak(ak)
@@ -23,7 +30,7 @@ if __name__ == '__main__':
     body = {
         "Type": "dir",
         "Prefix": True,
-        "Urls": url
+        "Urls": urls_string
     }
 
     resp = svc.submit_refresh_task(body)
